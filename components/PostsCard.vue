e359bcdb333d416a9f23aa2e161cbb0f740673ab
zcronin <zcronin1@gmail.com>
2025-10-30 23:10:05 -0600

    Adds post card

---
+++ components/PostsCard.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="grid gap-6">
+        <div class="col-span-full rounded-2xl border border-emeraldJade/30 bg-emeraldSpruce/70 p-6 shadow">
+            <slot name="intro" />
+        </div>
+        <div class="grid gap-6 sm:grid-cols-2">
+            <slot name="card" />
+        </div>
+    </div>
+</template>
